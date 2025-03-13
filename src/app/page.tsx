fe15f8244facd4ef187d8ff7a0defdc2ec005bc9
DylanLakhdari <emmelf.exe@gmail.com>
2025-03-13 16:05:03 +0100

    Database with Docker + Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
 import Button from "@/components/Button";
+import {prisma} from "@/lib/db";
+import {addTask} from "@/lib/actions";
 
-export default function Home() {
+export default async function Home() {
+
+    const tasks = await prisma.task.findMany();
+
     return (
-        <div className="flex flex-col items-center space-y-4 mt-10">
-            <h1>Example of component variants</h1>
-            <Button variant="primary">Primary Button</Button>
-            <Button variant="secondary">Secondary Button</Button>
-            <Button variant="tertiary">Tetiary Button</Button>
-        </div>
+        <>
+            <div className="flex flex-col items-center space-y-4 mt-10">
+                <h1>Example of component variants</h1>
+                <Button variant="primary">Primary Button</Button>
+                <Button variant="secondary">Secondary Button</Button>
+                <Button variant="tertiary">Tetiary Button</Button>
+            </div>
+            <div className="flex flex-col items-center space-y-4 mt-10">
+                <h1>Example of database usage</h1>
+                <form action={addTask} className="space-x-2 h-4">
+                    <input type="text" name="title" className="px-3 py-1 rounded border-1"/>
+                    <Button type="submit" variant="primary">Add task</Button>
+                </form>
+            </div>
+            <div className="flex flex-col items-center space-y-4 mt-10">
+                <h2>Liste des tâches</h2>
+                {tasks.length > 0 ? (
+                    <ul className="text-center">
+                        {tasks.map((task) => (
+                            <li key={task.id} className="mb-1">
+                                {task.title}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Aucune tâche trouvée.</p>
+                )}
+            </div>
+        </>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,14 @@
-// @ts-ignore
-export default function Button({ children, variant = "primary" }) {
-  const styles = "px-4 py-2 rounded-md font-medium transition-all";
-  const variants = {
-    primary: "bg-blue-500 text-white hover:bg-blue-600",
-    secondary: "bg-gray-500 text-white hover:bg-gray-600",
-    tertiary: "bg-red-500 text-white hover:bg-red-600",
-  };
+export default function Button({children="Button", variant = "primary", type="submit"}) {
+    const styles = "px-4 py-2 rounded-md font-medium transition-all";
+    const variants = {
+        primary: "bg-blue-500 text-white hover:bg-blue-600",
+        secondary: "bg-gray-500 text-white hover:bg-gray-600",
+        tertiary: "bg-red-500 text-white hover:bg-red-600",
+    };
 
-  return (  // @ts-ignore
-    <button className={`${styles} ${variants[variant]}`}>
-      {children}
-    </button>
-  );
+    return (  // @ts-ignore
+        <button type={type} className={`${styles} ${variants[variant]}`}>
+            {children}
+        </button>
+    );
 }
