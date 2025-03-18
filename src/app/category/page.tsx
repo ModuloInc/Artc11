5f73f9f809130fb9e5d1c6613676820be849b69b
DylanLakhdari <emmelf.exe@gmail.com>
2025-03-18 22:36:21 +0100

    feat(category): add category route and page, refactor NavBar category link

---
+++ src/app/category/page.tsx
@@ -0,0 +1,10 @@
+export default async function Category() {
+
+    return (
+        <>
+            <div className="flex flex-col items-center space-y-4 mt-10">
+                <h1 className="text-4xl font-extrabold">Category Page</h1>
+            </div>
+        </>
+    );
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -41,7 +41,7 @@ const BottomNavbar = ({ items }: BottomNavbarProps) => {
             ),
         },
         {
-            path: '/categorie',
+            path: '/category',
             icon: (
                 <svg width="28" height="24" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="12.2284" height="10.6651" rx="3" transform="matrix(-1 0 0 1 28 0)" fill="black"/>
